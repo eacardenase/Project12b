1b2f9dc0bf5739f546dae215181cc9f8f77f5550
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-14 17:41:26 -0500

    Adding photo and name to each collection item based on Person model data

--- Project10/Controllers/ViewController.swift
+++ Project10/Controllers/ViewController.swift
@@ -25,9 +25,37 @@ class ViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else { fatalError("Unable to dequeue PersonCell") }
         
-        cell.name.text = "\(indexPath.item)"
+        let person = people[indexPath.item]
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        
+        cell.name.text = person.name
+        cell.imageView.image = UIImage(contentsOfFile: path.path(percentEncoded: true))
+        cell.imageView.contentMode = .scaleAspectFill
+        cell.imageView.layer.borderColor = UIColor.black.cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Add person name", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak collectionView, weak person, weak ac] _ in
+            guard let personName = ac?.textFields?[0].text else { return }
+            
+            person?.name = personName
+            
+            collectionView?.reloadData()
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
     }
     
     @objc func addNewPerson() {
