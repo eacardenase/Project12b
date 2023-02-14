58be6a77dff7e1c9cb80a3ee15a81d4b73807a12
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-14 14:30:46 -0500

    Setting up UICollectionViewController

---
+++ Project10/PersonCVC.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCVC.swift
+//  Project10
+//
+//  Created by Edwin Cardenas on 2/14/23.
+//
+
+import UIKit
+
+class PersonCVC: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCVC else { fatalError("Unable to dequeue PersonCell") }
+        
+        cell.name.text = "\(indexPath.item)"
+        
+        return cell
+    }
 
 }
 
